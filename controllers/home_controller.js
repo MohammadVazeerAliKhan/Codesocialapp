6ab7f7e81ac7c680920425fc6606e348a1500831
Vazeer Khan <vazeerkhan511@gmail.com>
2023-06-25 09:58:21 +0530

    Adding view engine to social app

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,9 @@
 module.exports.home = function(req, res){
-  return res.end('<h1>Express is up  for codeSocialAPP</h1>');
+  return res.render('home',{
+    title: 'Home'
+  });
 }
 
 module.exports.post = function(req, res){
   return res.end('<h1>Loading posts from home_controller page</h1>');
-}
+}  
--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const port = 8000;
 // Use express router as middleware
 app.use('/',require('./routes/index'));
 
+// Setting ejs for sending back html to browser
+app.set('view engine','ejs');
+app.set('views','./views');
+
+
+// connecting server using port
 app.listen(port, function(err){
   if (err){
    console.log(`Error connecting to server ${err}`);
